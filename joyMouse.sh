90d0f61a3f36d26ddc39970f1555e88fb183aec8
Nicholas Peterson <derstreber2@gmail.com>
2016-01-13 11:41:17 -0600

    Create joyMouse.sh

---
+++ joyMouse.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+#joyMouse.sh
+#author: unknown (found it online)
+#description: View joystick and mouse events
+
+#first you must install the following
+# sudo apt-get install xserver-xorg-input-joystick
+if [ "$1" == "d" ]; then
+	echo Disabling gamepad as mouse device.
+	xinput --set-prop "9" "Device Enabled" 0
+elif [ "$1" == "l" ]; then
+	xinput list
+else
+	echo Enabling gamepad as mouse device.
+	xinput --set-prop "9" "Device Enabled" 1
+fi
